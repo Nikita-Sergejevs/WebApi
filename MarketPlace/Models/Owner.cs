22940c84d7fc206d7420f761036410806a3e5a8b
Keanky <n.sergejevs04@gmail.com>
2025-03-19 12:55:50 +0700

    Main models

--- MarketPlace/Models/Owner.cs
+++ MarketPlace/Models/Owner.cs
@@ -3,7 +3,7 @@ namespace MarketPlace.Models
     public class Owner
     {
         public int Id { get; set; }
-        public double Distance { get; set; }
+        public int Distance { get; set; }
         public string Name { get; set; } = string.Empty;
         public ICollection<ItemOwner> ItemOwners { get; set; } = null!;
     }
--- MarketPlace/Seed.cs
+++ MarketPlace/Seed.cs
@@ -7,7 +7,12 @@ namespace MarketPlace.Data
     {
         public static void Initialize(DataContext context)
         {
-            if(context.Item.Any() || context.Categories.Any()) return;
+            if(context.Item.Any() || context.Categories.Any() || context.Owners.Any()) return;
+
+            Random random = new Random();
+
+            int randomWorldRange = random.Next(1, 49);
+            int randomNetherRange = random.Next(50, 200);
 
             var categories = new List<Category>
             {
@@ -20,12 +25,23 @@ namespace MarketPlace.Data
 
             var item = new List<Item>
             {
-                new Item { Id = 0, Name = "" }
+                new Item { Id = 0, Name = "" , Price = 1}
             };
 
             var owner = new List<Owner>
             {
+                //WorldRange distance
+                new Owner { Id = 0, Distance = randomWorldRange, Name = "Steve" },
+                new Owner { Id = 1, Distance = randomWorldRange, Name = "Alex" },
+                new Owner { Id = 2, Distance = randomWorldRange, Name = "Iron Golem" },
+                new Owner { Id = 3, Distance = randomWorldRange, Name = "Villager" },
+                new Owner { Id = 4, Distance = randomWorldRange, Name = "Pillager" },
 
+                //NetherRange distance
+                new Owner { Id = 5, Distance = randomNetherRange, Name = "Witch" },
+                new Owner { Id = 6, Distance = randomNetherRange, Name = "Piglin" },
+                new Owner { Id = 7, Distance = randomNetherRange, Name = "Enderman" },
+                new Owner { Id = 8, Distance = randomNetherRange, Name = "Zombie" }
             };
 
             var reviews = new List<Review>
